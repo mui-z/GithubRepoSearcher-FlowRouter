f13436da36c9767cffa44dc30e4e352d7dd0a9e6
mui-z <93278577+mui-z@users.noreply.github.com>
2022-11-13 21:33:14 +0900

    fix

--- GithubRepoSearcherPackage/Sources/GithubRepoSearcherPackage/UI/Router/SearchFlowRouter.swift
+++ GithubRepoSearcherPackage/Sources/GithubRepoSearcherPackage/UI/Router/SearchFlowRouter.swift
@@ -8,7 +8,12 @@
 import Foundation
 import SwiftUI
 
-final class SearchFlowRouter: ObservableObject, FlowRouter {
+final class SearchFlowRouter: ObservableObject, FlowRouter, Hashable {
+    static func ==(lhs: SearchFlowRouter, rhs: SearchFlowRouter) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    private let id = UUID().uuidString
 
     @Published
     var navigationPath: NavigationPath = .init()
@@ -23,18 +28,22 @@ final class SearchFlowRouter: ObservableObject, FlowRouter {
     func nextTransitionScreen() -> some View {
         nextTransitionRoute.view
     }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
 }
 
 extension SearchFlowRouter {
     enum PushRoute: Hashable {
         case unknown
-        case detail(repo: GithubRepo)
+        case detail(repo: GithubRepo, router: SearchFlowRouter)
 
         @ViewBuilder
         var view: some View {
             switch self {
-            case .detail(let repo):
-                DetailScreen(repo: repo)
+            case .detail(let repo, let router):
+                DetailScreen(repo: repo, router: router)
             case .unknown:
                 fatalError("no set next transition screen.")
             }
